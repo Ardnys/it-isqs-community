9c2b9b36908fef6e73f0b7419a7ca7a396e3657e
Ardnys <semresavas35@gmail.com>
2025-04-22 00:12:08 +0300

    deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/it-isqs-community',
   envDir: './env',
   resolve: {
     alias: {
