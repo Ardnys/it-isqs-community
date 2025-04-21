606bb29f3b03f1e5c9f3b2cc6b53a71203d94b6a
Ardnys <semresavas35@gmail.com>
2025-04-22 01:03:24 +0300

    oh no i forgor base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/it-isqs-community',
   envDir: './env',
   resolve: {
     alias: {
