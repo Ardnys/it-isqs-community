61ad06a249cdd23f86922f94056f3e1ea19030ad
ThePotatoDuke <gameacedamy@gmail.com>
2025-04-24 20:24:14 +0300

    meh

--- src/mantine/modals/FIleUploadModal.tsx
+++ src/mantine/modals/FIleUploadModal.tsx
@@ -50,28 +50,6 @@ export const Upload = ({
 
   return (
     <Stack style={{ position: 'relative' }}>
-      <Group>
-        <Title order={2}>Materials</Title>
-        <MotionIcon
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          initial={{ y: 40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ type: 'spring', stiffness: 100 }}
-        >
-          <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-          <path d="M3 9h18" />
-          <path d="M9 21V9" />
-        </MotionIcon>
-      </Group>
-
       {innerProps.modalBody}
 
       <FileInput
